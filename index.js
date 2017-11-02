009ac5cfc383fa727b15c2aa51801c4aba06627a
Steven <steven_moxon123@hotmail.co.uk>
2017-11-02 10:51:49 +0000

    [_.last] add function

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ _.first = (list) => {
         return undefined;
     }
 };
+_.last = (list, n) => {
+    if (Array.isArray(list)) {
+      if (!n) {
+        return list[list.length - 1];
+      }
+      return list.slice(-n);
+    }
+  
+    if (typeof (list) === 'string') {
+      if (!n) {
+        return list[list.length - 1];
+      }
+      return list.split('').slice(-n);
+    }
+  };
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
