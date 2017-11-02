ceca85d5a8db23e4c264618dc2fcb5212ab9b12e
Steven <steven_moxon123@hotmail.co.uk>
2017-11-02 10:54:24 +0000

    [_.each] add function

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ _.last = (list, n) => {
       return list.split('').slice(-n);
     }
   };
+  _.each = (list, iteratee, context) => {
+    const thisParam = context || this;
+  
+    if (!Array.isArray(list) && typeof (list) !== 'string') {
+      for (var key in list) {
+        iteratee.call(thisParam, list[key], key, list);
+      }
+      return list;
+    }
+    else {
+  
+      for (var i = 0; i < list.length; i++) {
+        iteratee.call(thisParam, list[i], i, list);
+      }
+      return list;
+    }
+  };
 if (typeof module !== 'undefined') {
     module.exports = _;
 }
