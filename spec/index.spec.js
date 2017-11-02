1c43267443a9158e6cf2222cb74f8da9dafcebc9
Steven <steven_moxon123@hotmail.co.uk>
2017-11-02 10:53:47 +0000

    [_.each] add tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const expect = require('chai').expect;
-// const sinon = require('sinon');
+const sinon = require('sinon');
 
 const _ = require(path.join(__dirname, '../'));
 
@@ -50,5 +50,45 @@ describe('_', () => {
           expect(_.last(undefined)).to.be.undefined;
         });
       });
-      
+      describe('#each', () => {
+        
+            it('iterates over an array returning the original array', () => {
+              let spy = sinon.spy();
+              _.each([1, 2, 3, 4], spy);
+              expect(spy.callCount).to.equal(4);
+              expect(_.each([1, 2, 3, 4], spy)).to.eql([1, 2, 3, 4]);
+              expect(spy.calledWithExactly(1, 0, [1, 2, 3, 4])).to.equal(true);
+              expect(spy.calledWithExactly(2, 1, [1, 2, 3, 4])).to.equal(true);
+              expect(spy.calledWithExactly(3, 2, [1, 2, 3, 4])).to.equal(true);
+              expect(spy.calledWithExactly(4, 3, [1, 2, 3, 4])).to.equal(true);
+            });
+            it('iterates over a string returning the original string', () => {
+              let spy = sinon.spy();
+              _.each('abcd', spy);
+              expect(spy.callCount).to.equal(4);
+              expect(_.each('abcd', spy)).to.eql('abcd');
+              expect(spy.calledWithExactly('a', 0, 'abcd')).to.equal(true);
+              expect(spy.calledWithExactly('b', 1, 'abcd')).to.equal(true);
+              expect(spy.calledWithExactly('c', 2, 'abcd')).to.equal(true);
+              expect(spy.calledWithExactly('d', 3, 'abcd')).to.equal(true);
+            });
+            it('iterates over an object returning the original object', () => {
+              let spy = sinon.spy();
+              _.each({ a: 1, b: 2, c: 3 }, spy);
+              expect(spy.callCount).to.equal(3);
+              expect(_.each({ a: 1, b: 2, c: 3 }, spy)).to.eql({ a: 1, b: 2, c: 3 });
+              expect(spy.calledWithExactly(1, 'a', { a: 1, b: 2, c: 3 })).to.equal(true);
+              expect(spy.calledWithExactly(2, 'b', { a: 1, b: 2, c: 3 })).to.equal(true);
+              expect(spy.calledWithExactly(3, 'c', { a: 1, b: 2, c: 3 })).to.equal(true);
+            });
+            it('binds the iteratee to the context object if passed', () => {
+              function iteratee() {
+                this.a = 500;
+              }
+              const context = { a: 1, b: 2, c: 3 };
+              _.each([1, 2, 3], iteratee, context);
+              expect(context.a).to.equal(500);
+        
+            });
+          });
 });
