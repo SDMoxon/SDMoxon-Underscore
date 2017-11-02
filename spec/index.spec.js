bc5f4b68f0a1d512919c157db0800944b2f8afe6
Steven <steven_moxon123@hotmail.co.uk>
2017-11-02 10:51:17 +0000

    [_.last] add tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -21,9 +21,6 @@ describe('_', () => {
         });
       });
       describe('#first', () => {
-        it('is a function', () => {
-          expect(_.first).to.be.a('function');
-        });
         it('returns the first value of an array or string', () => {
           expect(_.first([1, 2, 3])).to.eql(1);
           expect(_.first('123')).to.eql('1');
@@ -36,4 +33,22 @@ describe('_', () => {
           expect(_.first(undefined)).to.be.undefined;
         });
       });
+      describe('#last', () => {
+        it('returns the last value of an array or string if n is not given', () => {
+          expect(_.last([1, 2, 3])).to.eql(3);
+          expect(_.last('123')).to.eql('3');
+        });
+        it('returns the last n elements if n is given', () => {
+          expect(_.last('123456789', 2)).to.eql(['8', '9']);
+          expect(_.last([1, 2, 3, 4, 5, 6, 7, 8, 9], 2)).to.eql([8, 9]);
+        });
+        it('returns undefined for all other data types', () => {
+          expect(_.last(12)).to.be.undefined;
+          expect(_.last(true)).to.be.undefined;
+          expect(_.last({ 1: 1, 2: 2 })).to.be.undefined;
+          expect(_.last(null)).to.be.undefined;
+          expect(_.last(undefined)).to.be.undefined;
+        });
+      });
+      
 });
