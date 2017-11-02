b354eddb2c4dcabd69a146dd3a70d96758cdd5fe
Steven <steven_moxon123@hotmail.co.uk>
2017-11-02 10:46:34 +0000

    [_.first] add tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -20,4 +20,20 @@ describe('_', () => {
           expect(_.identity(1)).to.equal(1);
         });
       });
+      describe('#first', () => {
+        it('is a function', () => {
+          expect(_.first).to.be.a('function');
+        });
+        it('returns the first value of an array or string', () => {
+          expect(_.first([1, 2, 3])).to.eql(1);
+          expect(_.first('123')).to.eql('1');
+        });
+        it('returns undefined for all other data types', () => {
+          expect(_.first(12)).to.be.undefined;
+          expect(_.first(true)).to.be.undefined;
+          expect(_.first({ 1: 1, 2: 2 })).to.be.undefined;
+          expect(_.first(null)).to.be.undefined;
+          expect(_.first(undefined)).to.be.undefined;
+        });
+      });
 });
